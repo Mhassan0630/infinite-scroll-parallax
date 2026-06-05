4c66fd2fbee9f34c08b6c75c63b45596a98a7bd0
Sam <sgvisualz@outlook.com>
2026-06-05 17:02:57 -0500

    Rename img folder to images

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+// LENIS SMOOTH SCROLL
+const lenis = new Lenis({
+  duration: 1.2,
+  smooth: true,
+  direction: "vertical",
+  gestureDirection: "vertical",
+  smoothTouch: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+// PARALLAX EFFECT
+gsap.utils.toArray(".hero").forEach((section, i) => {
+  gsap.fromTo(
+    section.querySelector("img"),
+    { scale: 1.2 },
+    {
+      scale: 1,
+      scrollTrigger: {
+        trigger: section,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+      },
+    }
+  );
+});
 
